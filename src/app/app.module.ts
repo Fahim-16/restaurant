c4532da4f2ccea9757902fc024eabac7cc91ab80
Fahim-16 <fahim.sakeer@gmail.com>
2022-07-07 20:36:16 +0530

    food view created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginpageComponent } from './loginpage/loginpage.component';
+import { SearchrestaurantComponent } from './searchrestaurant/searchrestaurant.component';
+import { ViewfoodComponent } from './viewfood/viewfood.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginpageComponent
+    LoginpageComponent,
+    SearchrestaurantComponent,
+    ViewfoodComponent
   ],
   imports: [
     BrowserModule,
